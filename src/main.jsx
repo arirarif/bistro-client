ff96388a629d39700a415227a2d7cbfe3d9895eb
arirarif <arirarif876@gmail.com>
2025-04-25 16:57:24 +0600

    header and footer

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,7 @@ import {
 import { router } from './Routes/Routes';
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <div className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={router} />
+  </div>
 );
